d5b1f5ced3eb92b908630bba8c404292e576232d
zhangweibin <zhangweibin@DESKTOP-KJI47G7>
2018-01-11 19:51:37 +0800

    1

---
+++ src/test/java/com/hbase/WinShopHour.java
@@ -0,0 +1,240 @@
+package com.hbase;
+
+/**
+ * win_shophour
+ */
+public class WinShopHour {
+
+	private String staffno;
+	private String type;
+	private String validbegin;
+	private String validend;
+	private String year;
+	private String area;
+	private String bs;
+	private String cameraid;
+	private String camerakey;
+	private String channelid;
+	private String closetime;
+	private String counterid;
+	private String customerid;
+	private String datatype;
+	private String day;
+	private String devicesn;
+	private String direction;
+	private String es;
+	private String hostname;
+	private String month;
+	private String opendate;
+	private String opentime;
+	private String relationid;
+	private String sitekey;
+	private String sitename;
+
+	public String getStaffno() {
+		return staffno;
+	}
+
+	public void setStaffno(String staffno) {
+		this.staffno = staffno;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValidbegin() {
+		return validbegin;
+	}
+
+	public void setValidbegin(String validbegin) {
+		this.validbegin = validbegin;
+	}
+
+	public String getValidend() {
+		return validend;
+	}
+
+	public void setValidend(String validend) {
+		this.validend = validend;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getBs() {
+		return bs;
+	}
+
+	public void setBs(String bs) {
+		this.bs = bs;
+	}
+
+	public String getCameraid() {
+		return cameraid;
+	}
+
+	public void setCameraid(String cameraid) {
+		this.cameraid = cameraid;
+	}
+
+	public String getCamerakey() {
+		return camerakey;
+	}
+
+	public void setCamerakey(String camerakey) {
+		this.camerakey = camerakey;
+	}
+
+	public String getChannelid() {
+		return channelid;
+	}
+
+	public void setChannelid(String channelid) {
+		this.channelid = channelid;
+	}
+
+	public String getClosetime() {
+		return closetime;
+	}
+
+	public void setClosetime(String closetime) {
+		this.closetime = closetime;
+	}
+
+	public String getCounterid() {
+		return counterid;
+	}
+
+	public void setCounterid(String counterid) {
+		this.counterid = counterid;
+	}
+
+	public String getCustomerid() {
+		return customerid;
+	}
+
+	public void setCustomerid(String customerid) {
+		this.customerid = customerid;
+	}
+
+	public String getDatatype() {
+		return datatype;
+	}
+
+	public void setDatatype(String datatype) {
+		this.datatype = datatype;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getDevicesn() {
+		return devicesn;
+	}
+
+	public void setDevicesn(String devicesn) {
+		this.devicesn = devicesn;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public String getEs() {
+		return es;
+	}
+
+	public void setEs(String es) {
+		this.es = es;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getOpendate() {
+		return opendate;
+	}
+
+	public void setOpendate(String opendate) {
+		this.opendate = opendate;
+	}
+
+	public String getOpentime() {
+		return opentime;
+	}
+
+	public void setOpentime(String opentime) {
+		this.opentime = opentime;
+	}
+
+	public String getRelationid() {
+		return relationid;
+	}
+
+	public void setRelationid(String relationid) {
+		this.relationid = relationid;
+	}
+
+	public String getSitekey() {
+		return sitekey;
+	}
+
+	public void setSitekey(String sitekey) {
+		this.sitekey = sitekey;
+	}
+
+	public String getSitename() {
+		return sitename;
+	}
+
+	public void setSitename(String sitename) {
+		this.sitename = sitename;
+	}
+
+	public WinShopHour(String sitekey) {
+		super();
+		this.sitekey=sitekey; 
+		
+	}
+
+}
--- src/test/java/com/hbase/hbaseTest.java
+++ src/test/java/com/hbase/hbaseTest.java
@@ -1,10 +1,18 @@
 package com.hbase;
 
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.hadoop.hbase.HbaseTemplate;
+import org.springframework.data.hadoop.hbase.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -14,17 +22,34 @@ public class hbaseTest {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
 
+	@Autowired
+	private HbaseTemplate hbaseTemplate;
+	
+	private String tableName = "win_shophour";
 
+	public static byte[] CF_INFO = Bytes.toBytes("cfInfo");
+
+	private byte[] qSitekey = Bytes.toBytes("sitekey");
+	
 
 	@Test
 	public void userRepositoryTest() {
+		//Apple_00001P00153
 		
+//		List<WinShopHour> list = hbaseTemplate.find(tableName, "cfInfo", new RowMapper<WinShopHour>() {
+//			@Override
+//			public WinShopHour mapRow(Result result, int rowNum) throws Exception {
+//				return new WinShopHour(Bytes.toString(result.getValue(CF_INFO, qSitekey)));
+//			}
+//		});
+		// List<Map<String,Object>>  mapList1 =hbaseTemplate.find(tableName, null, null);
 		
 		
 
-System.out.println("2323");
+ //  System.out.println(list.size());
 	}
 
 
+	
 
 }
